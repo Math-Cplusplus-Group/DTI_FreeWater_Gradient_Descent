fd475ce99dde1f2bfc20e079fa96e4d06a926aaf
MathCplusplusGroupAdmin <tkojar@math.toronto.edu>
2016-07-15 10:40:14 -0700

    Update CSVintomatrix.h

--- CSVintomatrix.h
+++ CSVintomatrix.h
@@ -1,6 +1,6 @@
 /** \file  CSVintomatrix.h
 \brief C++ source file initializing matrices from csv.
-Copyright 2016 by Tomas Kojar
+Copyright 2016 by Andrew Colinet, Tomas Kojar
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided
 that the following conditions are met:
