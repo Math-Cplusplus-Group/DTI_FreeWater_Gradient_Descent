3d7c3570efc1e95a0c8ef39b981d11dd393f3a52
MathCplusplusGroupAdmin <tkojar@math.toronto.edu>
2016-07-15 10:44:24 -0700

    Update EmbedInitial.h

--- EmbedInitial.h
+++ EmbedInitial.h
@@ -1,6 +1,6 @@
 /** \file  EmbedInitial.h
 \brief C++ header file initializing embedding map X.
-Copyright 2016 by Tomas Kojar
+Copyright 2016 by Andrew Colinet,Tomas Kojar
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided
 that the following conditions are met:
