e47dd9b6e38ca487010d0f87f744a6d900cb4732
MathCplusplusGroupAdmin <tkojar@math.toronto.edu>
2016-07-15 10:42:53 -0700

    Update ELInitialization.h

--- ELInitialization.h
+++ ELInitialization.h
@@ -1,6 +1,6 @@
 /** \file ELInitialization.h
 \brief C++ header file initializing physical variables.
-Copyright 2016 by Tomas Kojar
+Copyright 2016 by Andrew Colinet,Tomas Kojar
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided
 that the following conditions are met:
