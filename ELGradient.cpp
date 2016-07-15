be2dd26a021f3fcb42f23d51b46a0f9aac39a839
MathCplusplusGroupAdmin <tkojar@math.toronto.edu>
2016-07-15 10:40:51 -0700

    Update ELGradient.cpp

--- ELGradient.cpp
+++ ELGradient.cpp
@@ -1,6 +1,6 @@
 /** \file  ELGradient.cpp
 \brief C++ source file for iterating the gradient descent scheme.
-Copyright 2016 by Tomas Kojar
+Copyright 2016 by Andrew Colinet, Tomas Kojar
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided
 that the following conditions are met:
