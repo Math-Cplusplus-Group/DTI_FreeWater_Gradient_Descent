690f76b17b4cf8c4b7ed83de7492a36ec642a11d
TKojar <tkojar@math.toronto.edu>
2016-07-16 21:15:31 -0700

    Hello World

--- EmbedInitial.h
+++ EmbedInitial.h
@@ -1,5 +1,6 @@
 /** \file  EmbedInitial.h
 \brief C++ header file initializing embedding map X.
+
 Copyright 2016 by Andrew Colinet,Tomas Kojar
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided
